864a62c243a4e78aa7fb162c0ab5f28dcc1945a4
Yousra <yousrahoumma92@gmail.com>
2023-04-20 19:50:23 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programm that prints a line with puts funtion
+ * Return: 0 (Success)
+ */
+int main(void)
+{       
+	 printf("with proper grammar, but the outcome is a piece of art,\n")
+         return (0);
+}
