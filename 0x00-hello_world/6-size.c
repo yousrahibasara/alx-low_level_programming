bd828b866b2556be0812d981aaea2833f71e8634
Yousra <yousrahoumma92@gmail.com>
2023-04-22 12:14:38 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float e;
+	float f;
 
 {printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 {printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
