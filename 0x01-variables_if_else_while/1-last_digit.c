c995dbf8af7826f0b44528ed3721298b8aa0f02a
Yousra <yousrahoumma92@gmail.com>
2023-04-22 19:54:05 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 ld = n % 10;
 if (ld > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, ld);
+printf("Last digit of 98 is 8 and is greater than 5", n, ld);
 }
 else if (ld == 0)
 {
-printf("Last digit of %d is %d and is 0", n, ld);
+printf("Last digit of -98 is 98 and is 0", n, ld);
 }
 else
 {
-printf("Last digit of %d is %d and less than 6 and not 0", n, ld);
+printf("Last digit of 980 is 0 and less than 6 and not 0", n, ld);
 }
 return (0);
 }
