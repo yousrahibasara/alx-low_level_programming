68d1f326ab5666b5deb967c5e4f2d888be7848a1
Yousra <yousrahoumma92@gmail.com>
2023-04-22 20:08:39 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %d is %d and is 0\n", n, ld);
 }
 else
 {
-printf("Last digit of %d is %d and less than 6 and not 0\n", n, ld);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 }
 return (0);
 }
