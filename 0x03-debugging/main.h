3d9052de4a6715ebf1870910519f6e825b2d49c8
Yousra <yousrahoumma92@gmail.com>
2023-05-01 17:50:28 -0700

    3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int conert_day(int month, int day);
+int is_leap_year(int year);
+void  print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
 
