62da6227ba545e167aed7a742ce8ccf6dd3a1d3f
Yousra <yousrahoumma92@gmail.com>
2023-05-04 14:15:11 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 {
 int index = 0;
 
-while (str[index] >= 'a' && str[index] <= 'z')
+while (!(str[index] >= 'a' && str[index] <= 'z'))
 {
 index++;
 if (str[index - 1] == ' ' ||
