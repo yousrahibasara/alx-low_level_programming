f2601e518d5a7db20a07dc2c310b1100fd27fe77
Yousra <yousrahoumma92@gmail.com>
2023-05-03 17:17:34 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *main - Entry point
+  *Return: always 0
+  */
+
+int main(void)
+
+{
+
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+
+}
+
