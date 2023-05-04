1680a18254c4f7917e1eda7256ab500b063fc6ba
Yousra <yousrahoumma92@gmail.com>
2023-05-04 15:29:43 -0700

    100-rot13.c"

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,12 +11,12 @@ char *rot13(char *s)
 int i;
 int j;
 char data1[] =
-"ABCDEFGHIJKLMNOPQRSTUVWXYZqbcdefghijklmnopqrstuvwxyz";
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char datarot[] =
 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 52; j++)
+for (j = 0; j < s2; j++)
 {
 if (s[i] == data1[j])
 {
