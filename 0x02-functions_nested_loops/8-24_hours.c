4219c2a963304e5167da52757388b0b56dd89c6b
Yousra <yousrahoumma92@gmail.com>
2023-05-07 12:01:39 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *  Return: ...
+ */
+
+void jack_bauer(void)
+
+{
+
+int a, b, c, d;
+
+for (a = 0; a <= 2; a++)
+{
+for (b = 0; a <= 9; b++)
+{
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+{
+for (c = 0; a <= 5; c++)
+{
+for (d = 0; a <= 9; d++)
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(58);
+putchar(c + '0');
+putchar(d + '0');
+putchar('\n');
+}
+}
+}
+}
+}
+}
