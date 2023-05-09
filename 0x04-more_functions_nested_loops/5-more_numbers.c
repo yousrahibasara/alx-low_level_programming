d0386466a1b2546ba0f62ce92951ff84d3734c2f
Yousra <yousrahoumma92@gmail.com>
2023-05-09 15:51:59 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *more_numbers - print 10 times the numbers since 0 to 14
+ * more_numbers - print 10 times the numbers since 0 to 14
  * Return: 10 times of the numbers since 0 up to 14
  */
 
