3b4e1fc0a83dcca19e920c01b10ca93d60c19482
Yousra <yousrahoumma92@gmail.com>
2023-05-09 14:17:03 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,15 +5,13 @@
 *@c: character to be tested
 *Return: 1 whether it is, 0 otherwise
 */
-int _isupper(int c);
+
+int _isupper(int c)
 {
-if ((c >= 'A') && (c <= 'Z'))
+
+if (c >= 65 && c <= 90)
 {
 return (1);
 }
-else
-{
 return (0);
 }
-}
-
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-int _putchar(char c)
+int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
