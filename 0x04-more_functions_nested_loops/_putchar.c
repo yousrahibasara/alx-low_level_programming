c2c6b35c87d56721f94f6e785e460e365244c9da
Yousra <yousrahoumma92@gmail.com>
2023-05-09 13:59:28 -0700

    main.h

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,11 +1,9 @@
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print 
- *
+ * @c: character to print 
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriatly
+ * On error,   -1 is returned, and erno is set appropriatly
  */
 int _putchar(char c)
 {
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-*main - header file for prototypes
-*void prototypes (void)
-*int prototype (int)
-*/
+#include <stdio.h>
 
+int _putchar(char c)
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
