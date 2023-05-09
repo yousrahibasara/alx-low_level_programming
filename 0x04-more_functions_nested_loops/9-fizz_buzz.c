f16583c7b65d2c58b0fb7a83854c69130702c2b6
Yousra <yousrahoumma92@gmail.com>
2023-05-09 16:50:17 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,15 @@ int a;
 
 for (a = 1; a <= 100; a++)
 {
-if ((x % 3 == 0) && (x % 5 == 0))
+if ((a % 3 == 0) && (a % 5 == 0))
 {
 printf("FizzBuzz");
 }
-else if (x % 3 == 0)
+else if (a % 3 == 0)
 {
 printf("Fizz");
 }
-else if (x %  == 0)
+else if (a % 5 == 0)
 {
 printf("Buzz");
 }
