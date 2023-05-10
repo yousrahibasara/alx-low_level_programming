ac41343a4cfecd8bdf11eb044f032f81a82d849b
Yousra <yousrahoumma92@gmail.com>
2023-05-09 17:05:23 -0700

    0-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - Write a function that prints a triangle
+ * @size : the size of the squares triangle
+ * Return: empty
+ */
+
+void print_triangle(int size)
+
+{
+
+int a, b, c;
+
+if (size <= 0)
+{
+putchar('\n');
+}
+else
+{
+for (a = 0; a < size; a++)
+{
+for (b = size - a; b > 1; b--)
+{
+putchar(32);
+}
+for (c = 0; c <= a; z++)
+{
+putchar(35);
+}
+putchar('\n');
+}
+}
+}
