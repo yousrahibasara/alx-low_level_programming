79acadea21261c58a77f2c5fafa4bd64fff3022b
Yousra <yousrahoumma92@gmail.com>
2023-05-13 17:08:11 -0700

    libmy.a

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_isupper - cheks if a character is uppercase or not
+*@c: character to be tested
+*Return: 1 whether it is, 0 otherwise
+*/
+
+int _isupper(int c)
+{
+
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
