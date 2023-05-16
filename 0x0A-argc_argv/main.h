47720206b4d7e08233cdd1315f4d42236b1bb064
Yousra <yousrahoumma92@gmail.com>
2023-05-16 08:38:18 -0700

    main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,14 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
+int _atoi(char *s);
+int main (int argc, char *argv[1]);
+int main (int __attribute__((unused)), char *argv[]);
+int check_num(char *str);
+int main (int argc, char *argv[1]);
+int main (int argc, char *argv[1]);
+int _atoi(char *s);
 int main (int argc, char *argv[1]);
 
 #endif /* MAIN_H */
