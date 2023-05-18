138aed2b359b0509047f4aa9ff188873f11d5dd2
Yousra <yousrahoumma92@gmail.com>
2023-05-17 17:21:59 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_tab - print an array of string
+ * @tab: the array
+ * Return: nothing
+ */
+
+void print_tab(char **tab)
+{
+int i;
+
+for (i = 0; tab[i] != NULL; i++)
+{
+printf("%s\n", tab[i];
+}
+}
+
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: 1 if an error occured, 0 otherwise
+ */
+int main(void)
+{
+char **tab;
+
+tab = strtow("	ALX School	#cisfun		");
+if (tab == NULL)
+{
+printf("Failed\n");
+return (1);
+}
+print_tab(tab);
+return (0);
+}
