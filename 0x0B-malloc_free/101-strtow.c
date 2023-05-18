5b3e00b90f2c7588a3c0755142e6cb15f323f24e
Yousra <yousrahoumma92@gmail.com>
2023-05-17 18:10:08 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,39 +1,103 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-/**
- * print_tab - print an array of string
- * @tab: the array
- * Return: nothing
- */
-
-void print_tab(char **tab)
-{
-int i;
-
-for (i = 0; tab[i] != NULL; i++)
-{
-printf("%s\n", tab[i];
-}
-}
-
+int word_len(char *str);
+int count_word(char *str);
+char **strtow(char *str);
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: 1 if an error occured, 0 otherwise
+ * word_len - ...
+ * @str: ..
+ * Return: the index marking thr end of the intial word
+ * pointed to by str
  */
-int main(void)
-{
-char **tab;
 
-tab = strtow("	ALX School	#cisfun		");
-if (tab == NULL)
+int word_len(char *str)
 {
-printf("Failed\n");
-return (1);
+int index = 0, len = 0;
+
+while (*(str + index) && *(str + index) != ' ')
+{
+len++;
+index++;
 }
-print_tab(tab);
-return (0);
+
+return (len);
+}
+
+/**
+ * count_word - ...
+ * @str: ..
+ * Return: the number of words cintained within str
+ */
+
+int count_word(char *str)
+{
+int index = 0, word = 0, len = 0;
+
+for (index = 0; *(str + index); index++)
+len++;
+
+for (index = 0; index < len; index++)
+{
+if (*(str + index) != ' ')
+{
+word++;
+index += word_len(str + index);
+}
+}
+
+return (word);
+}
+
+
+/**
+ * strtow - ...
+ * @str: ..
+ * Return: if str = NULL, str = "", or the function fails - NULL
+ * otherwise - a pointer to an array of string
+ */
+
+char **strtow(char *str)
+{
+char **string;
+int index = 0, word, w, letter, l;
+
+if (str ==  NULL || str[0] == '\0')
+return (NULL);
+
+word = count_word(str);
+if (word == 0)
+return (NULL);
+
+string = malloc(sizeof(char *) * (word + 1));
+if (string == NULL)
+return (NULL);
+
+for (w = 0; w < word; w++)
+{
+while (str[index] == ' ')
+index++;
+
+letter = word_len(str + index);
+
+string[w] = malloc(sizeof(char) * (letter + 1));
+
+if (string[w] == NULL)
+{
+for (; w >= 0; w--)
+free(string[w]);
+
+free(string);
+return (NULL);
+}
+
+for (l = 0; l < letter; l++)
+string[w][l] = str[index++];
+
+string[w][l] = '\0';
+}
+string[w] = NULL;
+
+return (string);
 }
