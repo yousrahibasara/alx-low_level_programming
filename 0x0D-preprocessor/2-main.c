6f90beb2942f62aa35e0e32a179b895afde70ca2
Yousra <yousrahoumma92@gmail.com>
2023-05-23 09:56:40 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -  prints the name of the file
+ * main - prints the name of the file
  * Return: always 0
  */
 
 int main(void)
 {
-printf("%s\n", "FILE");
+printf("%s\n", "_FILE_");
 return (0);
 }
