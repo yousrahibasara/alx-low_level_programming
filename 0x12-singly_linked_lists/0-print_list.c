9ac8d82443d29e00eb4b02e7c5c263cf9c9db073
yousrahibasara <yousrahoumma@gmail.com>
2023-06-02 15:11:20 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,20 @@
 
 size_t print_list(const list_t *h)
 {
+
 size_t nodes = 0;
+
 while (h)
 {
 if (!h->str)
 printf("[0] (nil)\n");
+
 else
 printf("[%u] %s\n", h->len, h->str);
+
 h = h->next;
 nodes++;
 }
+
 return (nodes);
 }
