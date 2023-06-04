2091c39edd01b21cbee2cdf2eb6f21ee06e94e32
yousrahibasara <yousrahoumma@gmail.com>
2023-06-03 17:42:10 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,52 +1,6 @@
 #include "variadic_functions.h"
-
-/**
- * format_char - formats character
- * @separator: ....
- * @ap: .....
- */
-
-void format_char(char *separator, va_list ap)
-{
-printf("%s%c", separator, va_arg(ap, int));
-}
-
-/**
- * format_int - formats integer
- * @separator: ...
- * @ap: .....
- */
-
-void format_int(char *separator, va_list ap)
-{
-printf("%s%d", separator, va_arg(ap, int));
-}
-
-/**
- * format_float - formats float
- * @separator: ....
- * @ap: .....
- */
-
-void format_float(char *separator, va_list ap)
-{
-printf("%s%f", separator, va_arg(ap, double));
-}
-
-/**
- * format_string - formats string
- * @separator: ....
- * @ap: .....
- */
-
-void format_string(char *separator, va_list ap)
-{
-char *str = va_arg(ap, char *);
-switch ((int)(!str))
-case 1:
-str = "(nil)";
-printf("%s%s", separator, str);
-}
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_all - print antything
@@ -91,5 +45,3 @@ i++;
 printf("\n");
 va_end(list);
 }
-
-
