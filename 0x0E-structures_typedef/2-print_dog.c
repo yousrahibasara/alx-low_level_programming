075b17822cb690ddfde426cf2ae6cf8c6f6c1608
yousrahibasara <yousrahoumma@gmail.com>
2023-06-04 16:53:15 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ if (d->name != NULL)
 printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)\n");
-printf("Age: %6.f\n", d->age);
+printf("Age: %.6f\n", d->age);
 if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
 else
