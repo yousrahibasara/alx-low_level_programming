77f4ec62af7342f5ba9464f558faf213d7d6c8dd
yousrahibasara <yousrahoumma@gmail.com>
2023-06-06 16:33:31 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,33 @@
 #include "lists.h"
 
+
 /**
- * print_listint - prints all the elements of a listint_t list.
- * @h: linked list
+ 
+ * print_listint - prints all the elements of a linked list
+ 
+ * @h: linked list of type listint_t to print
+ 
  * Return: number of nodes
+ 
  */
 
 size_t print_listint(const listint_t *h)
-{
-size_t num = 0;
 
-while (h != NULL)
 {
-printf("%d\n", h->n);
-h = h->next;
-num++;
-}
 
-return (num);
+	size_t num = 0;
+
+	while (h)
+
+	{
+		printf("%d\n", h->n);
+
+		num++;
+
+		h = h->next;
+
+	}
+
+	return (num);
 
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,79 +1,37 @@
-#include <stdio.h>
 #include "lists.h"
 
-size_t looped_listint_len(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-
 /**
- * looped_listint_count - count the number
- * @head: ...
- * Return: ...
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
  */
 
-size_t looped_listint_count(listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-listint_t *tortoise, *hare;
-size_t nodes = 1;
-if (head == NULL || head->next == NULL)
+size_t len = 0;
+int diff;
+listint_t *temp;
+if (!h || !*h)
 return (0);
-tortoise = head->next;
-hare = (head->next)->next;
-while (hare)
+while (*h)
 {
-if (tortoise == hare)
+diff = *h - (*h)->next;
+if (diff > 0)
 {
-tortoise = head;
-while (tortoise != hare)
+temp = (*h)->next;
+free(*h);
+*h = temp;
+len++;
+}
+else
 {
-nodes++;
-tortoise = tortoise->next;
-hare = hare->next;
+free(*h);
+*h = NULL;
+len++;
+break;
 }
-tortoise = tortoise->next;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
 }
-return (nodes);
-}
-tortoise = tortoise->next;
-hare = (hare->next)->next;
-}
-return (0);
-}
-
-/**
- * free_listint_safe - ...
- * @h: ...
- * Return: ...
- * Description: the function sets that was freed
- */
-
-size_t free_listint_safe(listint_t **h);
-{
-	listint_t *tmp;
-	size_t nodes, index;
-	nodes = looped_listint_count(*h);
-	if (nodes == 0)
-	{
-		for (; h != NULL && *h != NULL; nodes++)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-		}
-	}
-	else
-	{
-		for (index = 0; index < nodes; index++)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-		}
-		*h = NULL;
-	}
-	h = NULL;
-	return (nodes);
+*h = NULL;
+return (len);
 }
