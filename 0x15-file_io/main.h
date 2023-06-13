0b4cfdff8feb41ac315480f8c4a63900625453fd
yousrahibasara <yousrahoumma@gmail.com>
2023-06-13 16:41:11 -0700

    main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#include "MAIN_H"
+#include "MAIN_H"
+
+#include <sys/types.h>
+#include <sys/start.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
