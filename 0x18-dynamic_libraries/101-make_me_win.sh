fb9141c590ba79a8c4f529d29162033dd0a3a2ed
yousrahibasara <yousrahoumma92@gmail.com>
2023-10-28 04:54:52 -0700

    1-create_dynamic_lib.sh

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int print (const char *format, ...)
+{
+	/*our code goes here
+	 * We can use write for printing text
+	 */
+
+	 write(1, "congratulations, you win the jackpoti*, 37);
+}
